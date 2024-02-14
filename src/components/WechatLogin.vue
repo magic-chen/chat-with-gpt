76db2066d018ddfcc19b63dafa2e14a64410c7f3
Murphy <chenlinping@laiye.com>
2024-02-14 23:14:20 +0800

    修复wechat页面逻辑

--- src/components/WechatLogin.vue
+++ src/components/WechatLogin.vue
@@ -10,10 +10,14 @@
 
     const router = useRouter();
     const code = ref(router.currentRoute.value.query.code)
+    const newcode = ref(router.currentRoute.value.query.newcode)
 
     onMounted(async () => {
         if(code.value != undefined){
-            await loginWithWechat(code.value as string)
+            console.log("重定向到wechat-login页面")
+            window.parent.location.href = `/wechat-login?newcode=${code.value}`;
+        }else if(newcode.value != undefined){
+            await loginWithWechat(newcode.value as string)
         }
     });
 </script>
